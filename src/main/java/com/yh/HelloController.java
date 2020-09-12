3e132903b64ccc8d62fedc5dedf8d6015c549883
yh <958619101@qq.com>
2020-09-12 17:05:57 +0800

    测试邮件

--- src/main/java/com/yh/HelloController.java
+++ src/main/java/com/yh/HelloController.java
@@ -1,9 +1,17 @@
 package com.yh;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.yh.entity.User;
+import com.yh.until.ExcelUtil;
+import com.yh.until.MailUtil;
+import com.yh.vo.testVo;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -13,5 +21,28 @@ public String  sayhello(){
         return "hello";
 }
 
+    public static void main(String[] args) throws Exception {
+
+        String sheetName="测试"+".xls";
+        File file=new File(sheetName);
+        Workbook workbook=new HSSFWorkbook() ;
+        ExcelUtil excelUtil=new ExcelUtil();
+        List<User> userList=  new ArrayList<>();
+        userList.add(new User("yh",19));
+        userList.add(new User("yh",19));
+        userList.add(new User("yh",19));
+        userList.add(new User("yh",19));
+        excelUtil.export(workbook,userList, testVo.class,sheetName);
+
+        FileOutputStream out=new FileOutputStream(sheetName);
+        workbook.write(out);
+        out.flush();
+        if (out!=null){
+            out.close();
+        }
+        String to="958619101@qq.com";
+        boolean send = MailUtil.send(to, "测试", sheetName);
+        System.out.println(send);
+    }
 
 }
